0a9a74abea15499dcf1021d094068ff2fce214cf
BiancaMesa <bianca.koraa@gmail.com>
2024-02-13 11:21:34 +0100

    class 3

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@
 
 //Crear la variable catList que es la clase list que sería el contenedor padre
 const catList = document.querySelector('.js-list');
+const inputSearchDesc = document.querySelector('.js_in_search_desc');
+const descrSearchText = inputSearchDesc.value;
+const messageFound = document.querySelector('.js-message');
 
 const kittenImage_1 = "https://dev.adalab.es/gato-siames.webp";
 const kittenName_1 = "Anastacio";
 const kittenDesc_1 = `Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos
 hace 500 años, donde tuvo su origen muy posiblemente.`;
-const kittenBreed_1 = "Siamés";
+const kittenBreed_1 = "";
 
 const kittenImage_2 = "https://dev.adalab.es/sphynx-gato.webp";
 const kittenName_2 = "Fiona";
@@ -25,6 +28,16 @@ const kittenBreed_3 = "Maine Coon";
 
 
 
+let html = ''; 
+
+//html lo estamos convirtiendo en KittenBreed_1
+if (kittenBreed_1 === "") {
+  html = 'Uy que despiste, no sabemos su raza'; 
+} else {
+  html = kittenBreed_1;
+}
+
+
 
 //Constante 1 creada en JS directamente
 
@@ -36,10 +49,12 @@ const kittenOne = `<li class="card">
     alt="siames-cat"
   />
   <h3 class="card_title">${kittenName_1}</h3>
-  <h4 class="card_race">${kittenBreed_1}</h4>
+  <h4 class="card_race">${html}</h4>
   <p class="card_description">${kittenDesc_1}</p>
 </article>
 </li>`;
+
+
 
 
 const kittenTwo = `<li class="card">
@@ -73,60 +88,27 @@ const kittenThree = `<li class="card">
 
 
 
-/*const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
-    alt="siames-cat"
-  />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-    Porte elegante, su patrón de color tan característico y sus ojos
-    de un azul intenso, pero su historia se remonta a Asía al menos
-    hace 500 años, donde tuvo su origen muy posiblemente.
-  </p>
-</article>
-</li>`;
-console.log(kittenOne);*/
 
-//Constante 2 creada en JS directamente
-/*const kittenTwo = `<li class="card">
-<img
-  class="card_img"
-  src="https://dev.adalab.es/sphynx-gato.webp"
-  alt="sphynx-cat"
-/>
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">Sphynx</h4>
-<p class="card_description">
-  Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-  hasta con pinta de alienígena han llegado a definir a esta raza
-  gatuna que se caracteriza por la «ausencia» de pelo.
-</p>
-</li>`;
-console.log(kittenTwo);*/
 
-//Constante 3 creada en JS directamente
-/*const kittenThree = `<li class="card">
-<img
-  class="card_img"
-  src="https://dev.adalab.es/maine-coon-cat.webp"
-  alt="maine-coon-cat"
-/>
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">Maine Coon</h4>
-<p class="card_description">
-  Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-  bella mirada se ha convertido en una de sus señas de identidad.
-  Sus ojos son grandes y las orejas resultan largas y en punta.
-</p>
-</li>`;
-console.log(kittenThree);*/
+if( kittenDesc_1.includes(descrSearchText) ) {
+  catList.innerHTML = kittenOne}
+
+if( kittenDesc_2.includes(descrSearchText) ) {
+  catList.innerHTML = kittenTwo;
+}
+
+if( kittenDesc_3.includes(descrSearchText) ) {
+  catList.innerHTML = kittenThree;
+}
+
+
+
+
+
+
 
 //Crear items dentro de la variable catList
-catList.innerHTML = kittenOne + kittenTwo + kittenThree;
+//catList.innerHTML = kittenOne + kittenTwo + kittenThree;
 
 
 
