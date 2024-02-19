82564d8fade9eff160a3bb733f59ba16c8f7604f
Alba Melchor Gomez <albamelchorgomez@gmail.com>
2024-02-19 11:19:44 +0100

    renderKitten

--- js/main.js
+++ js/main.js
@@ -9,83 +9,46 @@ const descrSearchText = inputSearchDesc.value;
 const messageFound = document.querySelector('.js-message');
 const searchButton = document.querySelector('.js-search-button');
 
-
-const kittenImage_1 = "https://dev.adalab.es/gato-siames.webp";
-const kittenName_1 = "Anastacio";
-const kittenDesc_1 = `Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos
-hace 500 años, donde tuvo su origen muy posiblemente.`;
-const kittenBreed_1 = "";
-
-const kittenImage_2 = "https://dev.adalab.es/sphynx-gato.webp";
-const kittenName_2 = "Fiona";
-const kittenDesc_2 = ` Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+const objKittenOne = {
+  image: "https://dev.adalab.es/gato-siames.webp",
+  name: "Anastacio",
+  desc: `Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos
+  hace 500 años, donde tuvo su origen muy posiblemente.`,
+  breed: "",
+}
+const objKittenTwo = {
+  image: "https://dev.adalab.es/sphynx-gato.webp",
+  name: "Fiona",
+  desc: ` Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
 hasta con pinta de alienígena han llegado a definir a esta raza
-gatuna que se caracteriza por la «ausencia» de pelo.`;
-const kittenBreed_2 = "Sphynx";
+gatuna que se caracteriza por la «ausencia» de pelo.`,
+  breed: "Sphynx",
+}
 
+const objKittenThree = {
+  image: "https://dev.adalab.es/maine-coon-cat.webp",
+  name: "Cielo",
+  desc: `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad.
+  Sus ojos son grandes y las orejas resultan largas y en punta.`,
+  breed: "Maine Coon",
+}
 
-const kittenImage_3 = "https://dev.adalab.es/maine-coon-cat.webp";
-const kittenName_3 = "Cielo";
-const kittenDesc_3 = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad.
-Sus ojos son grandes y las orejas resultan largas y en punta.`;
-const kittenBreed_3 = "Maine Coon";
 
 let html = '';
 
 //html lo estamos convirtiendo en KittenBreed_1
-if (kittenBreed_1 === "") {
+if (objKittenOne.breed === "") {
   html = 'Uy que despiste, no sabemos su raza';
 } else {
-  html = kittenBreed_1;
+  html = objKittenOne.breed;
 }
-
-//Constante 1 creada en JS directamente
-
-const kittenOne = `<li class="card js-kit1" >
-<article>
-  <img
-    class="card_img"
-    src= ${kittenImage_1}
-    alt="siames-cat"
-  />
-  <h3 class="card_title">${kittenName_1}</h3>
-  <h4 class="card_race">${html}</h4>
-  <p class="card_description">${kittenDesc_1}</p>
-</article>
-</li>`;
-
-
-const kittenTwo = `<li class="card  js-kit2" >
-<article>
-  <img
-    class="card_img"
-    src= ${kittenImage_2}
-    alt="siames-cat"
-  />
-  <h3 class="card_title">${kittenName_2}</h3>
-  <h4 class="card_race">${kittenBreed_2}</h4>
-  <p class="card_description">${kittenDesc_2}</p>
-</article>
-</li>`;
-
-
-const kittenThree = `<li class="card js-kit3" >
-<article>
-  <img
-    class="card_img"
-    src= ${kittenImage_3}
-    alt="siames-cat"
-  />
-  <h3 class="card_title">${kittenName_3}</h3>
-  <h4 class="card_race">${kittenBreed_3}</h4>
-  <p class="card_description">${kittenDesc_3}</p>
-</article>
-</li>`;
 
 
 //Crear items dentro de la variable catList
 
-catList.innerHTML = kittenOne + kittenTwo + kittenThree;
+catList.innerHTML = renderKitten(objKittenOne) + renderKitten(objKittenTwo) + renderKitten(objKittenThree);
+
+console.log();
 
 //crear nuevas variables que obtengan el section ya impreso
 
@@ -100,20 +63,20 @@ const handleFilterKitten = (event) => {
   event.preventDefault();
 
   catList.innerHTML = '';
-  
+
 
   if (kittenDesc_1.includes(inputSearchDesc.value)) {
-    
+
     catList.innerHTML += kittenOne;
   }
-  
+
   if (kittenDesc_2.includes(inputSearchDesc.value)) {
-  
+
     catList.innerHTML += kittenTwo;
   }
-  
+
   if (kittenDesc_3.includes(inputSearchDesc.value)) {
-  
+
     catList.innerHTML += kittenThree;
   }
 }
@@ -124,6 +87,7 @@ searchButton.addEventListener('click', handleFilterKitten);
 
 console.log(descrSearchText.value);
 
+//DOM Formulario de añadir gatos
 
 const formSection = document.querySelector('.js-new-form');
 const inputDesc = document.querySelector('.js-input-desc');
@@ -163,16 +127,18 @@ function handleClickNewCatForm(event) {
 }
 
 
-formButton.addEventListener('click',handleClickNewCatForm);
-
-
-
-
-
+formButton.addEventListener('click', handleClickNewCatForm);
 
 btnAdd.addEventListener('click', (event) => {
 
   event.preventDefault();
+  addNewKitten();
+
+});
+
+
+function addNewKitten() {
+
   const valueDesc = inputDesc.value;
   const valuePhoto = inputPhoto.value;
   const valueName = inputName.value;
@@ -183,11 +149,34 @@ btnAdd.addEventListener('click', (event) => {
     labelMessageError.innerHTML = "Uy! parece que has olvidado algo"
 
   } else {
-
+    //aquí se creara el gato
     labelMessageError.innerHTML = "Correcto"
   }
 
-});
+
+
+}
+function renderKitten(numberCat) {
+
+  const rendKitten = `<li class="card js-kit3" >
+  <article>
+    <img
+      class="card_img"
+      src= ${numberCat.image}
+      alt="siames-cat"
+    />
+    <h3 class="card_title">${numberCat.name}</h3>
+    <h4 class="card_race">${numberCat.breed}</h4>
+    <p class="card_description">${numberCat.desc}</p>
+  </article>
+  </li>`;
+
+  return rendKitten;
+}
+
+
+
+
 
 //habia que cerrar el menu no decirle que se borre jeje
 
